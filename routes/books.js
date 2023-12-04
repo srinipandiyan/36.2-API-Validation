787139a08815dde753035e195c3e1ff58c8fea7f
Srini Pandiyan <srinipandiyan7@gmail.com>
2023-12-03 23:17:54 -0600

    JSONschemas created and validation implemented in routes

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const Book = require("../models/book");
-
 const router = new express.Router();
+const { validate } = require("jsonschema");
+const Book = require("../models/book");
+const newBookSchema = require("../schemas/newBookSchema");
+const updateBookSchema = require("../schemas/updateBookSchema");
 
 
 /** GET / => {books: [book, ...]}  */
@@ -17,9 +19,9 @@ router.get("/", async function (req, res, next) {
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
+router.get("/:isbn", async function (req, res, next) {
   try {
-    const book = await Book.findOne(req.params.id);
+    const book = await Book.findOne(req.params.isbn);
     return res.json({ book });
   } catch (err) {
     return next(err);
@@ -30,7 +32,14 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
-    const book = await Book.create(req.body);
+    const validation = validate(req.body, newBookSchema)
+    if (!validation.valid){
+      return next({
+        status: 400,
+        errors: validation.errors.map(err = err.stack)
+      });
+    }
+    const book = Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
     return next(err);
@@ -41,6 +50,19 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    if ("isbn" in req.body){
+      return next ({
+        status: 400,
+        message: "The isbn not allowed in request body."
+      })
+    }
+    const validation = validate(req.body, updateBookSchema)
+    if (!validation.valid){
+      return next({
+        status: 400,
+        errors: validation.errors.map(err = err.stack)
+      });
+    }
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
